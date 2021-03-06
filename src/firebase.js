640083ed7261556e3aac1f485ecaf70788147984
adam040992 <adam040992@gmail.com>
2021-03-06 15:17:01 +0100

    Starting-project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBG2DzpLI94uR2oBYh7760cXgA1vi3eL_k",
+    authDomain: "slack-clone-abcce.firebaseapp.com",
+    projectId: "slack-clone-abcce",
+    storageBucket: "slack-clone-abcce.appspot.com",
+    messagingSenderId: "377132914242",
+    appId: "1:377132914242:web:41abb38a5ed8c730753fdb"
+  };
