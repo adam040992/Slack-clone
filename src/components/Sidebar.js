def353bbbe8b3795b7c92be17de7ce9e95594941
adam040992 <adam040992@gmail.com>
2021-03-07 13:47:49 +0100

    adding Sidebar props, reusable element

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord'
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from '@material-ui/icons/Create'
+import SidebarOption from './SidebarOption';
+import InsertCommentIcon from '@material-ui/icons/InsertComment'
+import InboxIcon from '@material-ui/icons/Inbox'
+import DraftsIcon from '@material-ui/icons/Drafts'
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
+import FileCopyIcon from '@material-ui/icons/FileCopy'
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt'
+import AppsIcon from '@material-ui/icons/Apps'
+import ExpandLessIcon from '@material-ui/icons/ExpandLess'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import AddIcon from '@material-ui/icons/Add'
 
 function Sidebar() {
     return (
@@ -13,9 +25,24 @@ function Sidebar() {
                        Adam W
                    </h3>
                </SidebarInfo>
+               <CreateIcon />
             </SidebarHeader>
+
+            <SidebarOption Icon={InsertCommentIcon} title='Threads'/>
+            <SidebarOption Icon={InboxIcon} title='Mentions & reacttons'/>
+            <SidebarOption Icon={DraftsIcon} title='Saved Items'/>
+            <SidebarOption Icon={BookmarkBorderIcon} title='Channel browser'/>
+            <SidebarOption Icon={PeopleAltIcon} title='People & user groups'/>
+            <SidebarOption Icon={AppsIcon} title='Apps'/>
+            <SidebarOption Icon={FileCopyIcon} title='File browser'/>
+            <SidebarOption Icon={ExpandLessIcon} title='Show less'/>
+            <hr />
+            <SidebarOption Icon={ExpandMoreIcon} title='Channels'/>
+            <hr />
+            <SidebarOption Icon={AddIcon} addChannelOption title='Add Channel'/>
+
         </SidebarContainer>
-    )
+    );
 }
 
 export default Sidebar
@@ -30,11 +57,42 @@ const SidebarContainer = styled.div`
 `;
 
 const SidebarHeader = styled.div`
+    display: flex;
+    border-bottom: 1px solid #49274b;
+    padding: 13px;
 
+    > .MuiSvgIcon-root {
+        padding: 8px;
+        color: #49274b;
+        font-size: 18px;
+        background-color: white;
+        border-radius: 999px;
+        align-self: center;
+    }
 `;
 
 const SidebarInfo = styled.div`
+    flex: 1;
 
+    > h2 {
+        font-size: 15px;
+        font-weight: 900;
+        margin-bottom: 5px;
+    }
+
+    > h3 {
+        display: flex;
+        font-size: 13px;
+        font-weight: 400;
+        align-items: center;
+    }
+
+    > h3 > .MuiSvgIcon-root {
+        font-size: 14px;
+        margin-top: 1px;
+        margin-right: 2px;
+        color: green;
+    }
 `;
 
 
