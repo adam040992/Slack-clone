60285bca6b7717a9ea2f2ae3396d07970ff429c5
adam040992 <adam040992@gmail.com>
2021-03-07 14:05:28 +0100

    Adding firebase datastore

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyBG2DzpLI94uR2oBYh7760cXgA1vi3eL_k",
     authDomain: "slack-clone-abcce.firebaseapp.com",
@@ -6,3 +8,7 @@ const firebaseConfig = {
     messagingSenderId: "377132914242",
     appId: "1:377132914242:web:41abb38a5ed8c730753fdb"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth.GoogleAuthProvider();
