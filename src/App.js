d8f8f72b6f3dd257b3d5ff0d997c8f2a8522d448
adam040992 <adam040992@gmail.com>
2021-03-10 18:25:50 +0100

    Added Loading indicator

--- src/App.js
+++ src/App.js
@@ -8,10 +8,29 @@ import Chat from './components/Chat';
 import Login from './components/Login';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './firebase';
+import Spinner from 'react-spinkit';
 
 
 function App() {
   const [user, loading] = useAuthState(auth);
+
+  if(loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContainer>
+            <img
+              src='https://cdn.worldvectorlogo.com/logos/slack-new-logo.svg'
+              alt='Slack Logo'  
+            />
+            <Spinner 
+              name="ball-spin-fade-loader" 
+              color="black"
+              fadeIn="none"
+            />
+        </AppLoadingContainer>
+      </AppLoading>
+    )
+  }
 
   return (
     <div className="app">
@@ -39,6 +58,28 @@ function App() {
 }
 
 export default App;
+
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`;
+
+const AppLoadingContainer = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
+`;
 
 const AppBody = styled.div`
   display: flex;
